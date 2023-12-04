043b4997bde4aa26119668e8efb47606b9073094
Iskita Gurung <iskita@none.none>
2023-12-04 18:17:47 -0500

    Add alt combat minigame

--- game/locations/iskitaisland.py
+++ game/locations/iskitaisland.py
@@ -4,18 +4,25 @@ import game.config as config
 from game.display import announce 
 from game.events import *
 import game.items as items
+import random
+import game.myis as caveb
 
 class Island (location.Location):
 
     def __init__ (self, x, y, w):
         super().__init__(x, y, w)
         self.name = "island"
-        self.symbol = 'I'
+        self.symbol = 'A'
         self.visitable = True
         self.starting_location = Beach_with_ship(self)
         self.locations = {}
         self.locations["beach"] = self.starting_location
         self.locations["trees"] = Trees(self)
+        self.locations["riddle_gate"]= Riddle_gate(self)
+        """self.locations["volcano"] = Volcano(self)
+        self.locations["bush"] = Bush(self)
+        self.locations["temple"] = Temple(self)
+        self.locations["waterfall"] = Waterfall(self)"""
 
     def enter (self, ship):
         print ("arrived at an island")
@@ -42,13 +49,16 @@ class Beach_with_ship (location.SubLocation):
 
     def process_verb (self, verb, cmd_list, nouns):
         if (verb == "south"):
+            config.the_player.next_loc = self.main_location.locations["trees"]
             announce ("You return to your ship.")
             config.the_player.next_loc = config.the_player.ship
             config.the_player.visiting = False
         elif (verb == "north"):
-            config.the_player.next_loc = self.main_location.locations["trees"]
-        elif (verb == "east" or verb == "west"):
-            announce ("You walk all the way around the island on the beach. It's not very interesting.")
+            config.the_player.next_loc = self.main_location.locations["riddle_gate"]
+        elif (verb == "west"):  
+            config.the_player.next_loc = self.main_location.locations["temple"] 
+        elif (verb == "east"):
+            config.the_player.next_loc = self.main_location.locations["bush"]
 
 
 class Trees (location.SubLocation):
@@ -88,7 +98,9 @@ class Trees (location.SubLocation):
 
     def process_verb (self, verb, cmd_list, nouns):
         if (verb == "south" or verb == "north" or verb == "east" or verb == "west"):
-            config.the_player.next_loc = self.main_location.locations["beach"]
+            announce ("You return to your ship.")
+            config.the_player.next_loc = config.the_player.ship
+            config.the_player.visiting = False
         #Handle taking items. Demo both "take cutlass" and "take all"
         if verb == "take":
             if self.item_in_tree == None and self.item_in_clothes == None:
@@ -112,58 +124,161 @@ class Trees (location.SubLocation):
                 if at_least_one == False:
                     announce ("You don't see one of those around.")
 
-class riddle_gate(location.SubLocation):  
-    def _init_(self):
+class Riddle_gate(location.SubLocation):  
+    def __init__(self,m):
         super().__init__(m)
-        self.name="ridde_gate"
+        self.name = "ridde_gate"
         self.verbs['north'] = self
         self.verbs['south'] = self
         self.verbs['east'] = self
         self.verbs['west'] = self
         self.event_chance = 10
+        self.woord="water"
         self.word=["w","a","t","e","r"]
+        self.words=self.word[:]
         self.item_after_puzzle = items.Cutlass()
         self.open_gate = False
+        random.shuffle(self.word)
+        self.words=self.word[:]
+
+
     def enter (self):
-        announce("WElcome! You have to rearrange the alphabet to open this gate ")
-        if self.open_gate = True
-    def process_verb (self, verb, cmd_list, nouns):   
-        if (verb == "south" or verb == "north" or verb == "east" or verb == "west"):
-            config.the_player.next_loc = self.main_location.locations["beach"]
-        jumble=''.join(self.word)
-        return jumble
-        an=jumble
+        announce ("WElcome! You have to rearrange the alphabet to open this gate. ")
         count1 = 0
         count2 = self.event_chance = 10
-        while count1 < count2:
-            print("your word is :", an)
-            user_word=input("your word : ")
+        while count1 < count2 and not self.open_gate:
+            print("rearrange this word:")
+            print(self.words)
+            user_word=input("your word : ", )
 
-            if user_word != an:
+            if user_word != self.woord: 
                 count1 +=1
+                print("incorrect")
             else:
                 print("correct!you got it ")  
                 item = self.item_after_puzzle
                 config.the_player.add_to_inventory([item])
-                self.item_in_tree = None
-                config.the_player.go = True
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                print("yay!!you got a cutlass")
+                self.item_in_puzzle = None
+                self.open_gate = True
+                print("the gate is open")
+                
+		
+   
+    def process_verb (self, verb, cmd_list, nouns):
+        if self.open_gate = True:
+            if (verb == "west" or verb == "north"):
+                config.the_player.next_loc = self.main_location.locations["volcano"]
+            elif(verb == "east" or verb == "south"):
+                config.the_player.next_loc = self.main_location.locations["cave"]
+        else:
+            if (verb == "south" or verb == "north" or verb == "east" or verb == "west"):
+                announce ("You return to your ship.")
+                config.the_player.next_loc = config.the_player.ship
+                config.the_player.visiting = False       
+            
+"""class Volcano(location.SubLocation):
+    def __init__ (self, m):
+        super().__init__(m)
+        self.name = "volcano"
+        self.verbs['north'] = self
+        self.verbs['south'] = self
+        self.verbs['east'] = self
+        self.verbs['west'] = self
+        self.item_after_quiz = items.Flintlock()
+        self.num1 = random.randint(1, 10) 
+        self.num2 = random.randint(1, 10)
+    def enter (self):
         
+        correct_answer = self.num1 + self.num2
+
+        print(f"Solve the Math Puzzle: {self.num1} + {self.num2}")
+        user_answer = int(input("Your answer: "))
+        if user_answer == correct_answer:
+            print("correct!you got it ")  
+            item = self.item_after_puzzle
+            config.the_player.add_to_inventory([item])
+            print("yay!!you got a flintlock")
+            self.item_in_quiz = None
+            config.the_player.go = True
+
+        else:
+           print("Incorrect! Try again")
+	
+
+    def process_verb (self, verb, cmd_list, nouns):   
+        if self.open_gate == True:
+            if (verb == "south" or verb == "north" or verb == "south" ):
+                announce ("You return to your ship.")
+                config.the_player.next_loc = config.the_player.ship
+                config.the_player.visiting = False
+            elif(verb == "east" ):
+                config.the_player.next_loc = self.main_location.locations["cave"]"""
+
+class Cave (location.SubLocation):
+    def __init__ (self, m):
+        super().__init__(m)
+        self.name = "cave"
+        self.verbs['north'] = self
+        self.verbs['south'] = self
+        self.verbs['east'] = self
+        self.verbs['west'] = self
+
+    def enter (self):
+        announce("WElcome to the cave!! you have to fight this creature to get the treasure ")
+        caveb
+
+    def process_verb (self, verb, cmd_list, nouns):   
+         if self.open_gate == True:
+            if (verb == "south" or verb == "north"):
+                config.the_player.next_loc = self.main_location.locations["volcano"]
+            elif(verb == "east" or verb == "south"):
+                config.the_player.next_loc = self.main_location.locations["cave"]
+
+"""class Temple(location.SubLocation):
+    def __init__ (self, m):
+        super().__init__(m)
+        self.name = "volcano"
+        self.verbs['north'] = self
+        self.verbs['south'] = self
+        self.verbs['east'] = self
+        self.verbs['west'] = self
+
+    def enter (self):
+        announce("WElcome! You have to rearrange the alphabet to open this gate ")
+    def process_verb (self, verb, cmd_list, nouns):
+            if (verb == "south"):
+                config.the_player.next_loc = self.main_location.locations["trees"]
+                announce ("You return to your ship.")
+                config.the_player.next_loc = config.the_player.ship
+                config.the_player.visiting = False
+            elif (verb == "north"):
+                config.the_player.next_loc = self.main_location.locations["riddle_gate"]
+            elif (verb == "west"):  
+                config.the_player.next_loc = self.main_location.locations["temple"] 
+            elif (verb == "east"):
+                config.the_player.next_loc = self.main_location.locations["bush"]
+
+class Bush(location.SubLocation):
+    def __init__ (self, m):
+        super().__init__(m)
+        self.name = "volcano"
+        self.verbs['north'] = self
+        self.verbs['south'] = self
+        self.verbs['east'] = self
+        self.verbs['west'] = self
+
+    def enter (self):
+        announce("WElcome! You have to rearrange the alphabet to open this gate ")
+    def process_verb (self, verb, cmd_list, nouns):
+            if (verb == "south"):
+                config.the_player.next_loc = self.main_location.locations["trees"]
+                announce ("You return to your ship.")
+                config.the_player.next_loc = config.the_player.ship
+                config.the_player.visiting = False
+            elif (verb == "north"):
+                config.the_player.next_loc = self.main_location.locations["riddle_gate"]
+            elif (verb == "west"):  
+                config.the_player.next_loc = self.main_location.locations["temple"] 
+            elif (verb == "east"):
+                config.the_player.next_loc = self.main_location.locations["bush"]"""                
---
+++ game/myis.py
@@ -0,0 +1,126 @@
+import random
+
+class CaveBattle:
+    
+     
+    def __init__(self): 
+        self.battle_continue = True
+       
+        
+    def attack(self):
+        if self.attack_choice == 1:
+            attack_points = random.randint(18,25)
+            return attack_points 
+        elif self.attack_choice == 2: 
+            attack_points = random.randint(10,35) 
+            return attack_points
+    def creatureattack(self):
+        if self.creature_choice == 1:
+            creature_points = random.randint(18,25)
+            return creature_points 
+        elif self.creature_choice == 2: 
+           creature_points = random.randint(10,35)
+           return creature_points
+        
+    def heal(self):
+        if self.attack_choice == 3:
+            
+            heal_points = random.randint(18,25) 
+            return heal_points
+    def creatureheal(self):
+        if self.creature_choice == 3:
+            
+            creatureheal_points = random.randint(18,25) 
+            return creatureheal_points     
+       
+        
+    def checkHealth(self):
+        if self.user_health > 100:
+            self.user_health = 100 
+        elif self.user_health <= 0: 
+            self.user_health = 0
+            self.battle_continue = False
+        
+        if self.creature_health > 100: 
+            self.creature_health = 100 
+        elif self.creature_health <= 0: 
+            self.creature_health = 0 
+            self.battle_continue = False 
+        
+             
+        
+    def playGame(self):
+        if self.attack_choice == 1 or self.attack_choice == 2:
+            self.damage_to_creature = self.attack()
+            self.heal_self = 0
+            if self.creature_choice == 1:
+                print("creature used close range attack")
+            elif self.creature_choice == 2:
+                print("creature used far range attack")
+            else:
+                print("creature healed itself")
+            print("You dealt",self.damage_to_creature,"damage.")
+            self.creature_health = (self.creature_health) - self.damage_to_creature
+            self.checkHealth()
+                
+        if self.creature_choice == 1 or self.creature_choice == 2:
+            self.damage_to_user = self.creatureattack()
+            self.heal_creature = 0
+            print("CREATURE dealt", self.damage_to_user, "damage.")
+            self.user_health = self.user_health - self.damage_to_user
+            self.checkHealth()
+                
+        if self.attack_choice == 3:
+            self.heal_self = self.heal() 
+            self.damage_to_creature = 0
+            if self.creature_choice == 1:
+                print("creature used close range attack")
+            elif self.creature_choice == 2:
+                print("creature used far range attack")
+            else:
+                print("creature healed itself")
+            print("You dealt",self.damage_to_creature,"damage.")
+            print("You healed",self.heal_self,"health points.")
+            self.user_health = self.user_health + self.heal_self
+            self.checkHealth()
+                
+        if self.creature_choice == 3:
+            self.heal_creature = self.creatureheal() 
+            self.damage_to_user = 0 
+            print("creature healed", self.heal_creature, "health points ")
+            
+            self.creature_health = self.creature_health + self.heal_creature
+            self.checkHealth()
+        
+    def process(self):    
+        self.user_health = 100
+        self.creature_health = 100
+        while self.battle_continue == True:
+            print("----------------------------------")
+            print("Your current health is", self.user_health)
+            print("creature's current health is", self.creature_health)  
+            print("\nATTACK CHOICES\n1. Close range attack\n2. Far range attack\n3. Heal")
+            self.attack_choice = eval(input("\nSelect an attack: "))
+            if self.creature_health == 100:
+                self.creature_choice = random.randint(1,2)
+            else:
+                self.creature_choice = random.randint(1,3)
+                   
+            self.playGame()
+            self.checkHealth()
+            
+
+            
+                
+        if self.user_health < self.creature_health: 
+            print("\nYou lost! Better luck next time!")
+        else:
+            print("\nYou won against creature!")
+                
+                       
+ 
+    
+            
+if __name__== "__main__":
+    game = CaveBattle()
+    game.process()
